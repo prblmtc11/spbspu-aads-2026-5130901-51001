7e8a5cd3536b413bbf0e337dbbaa9469c06037af
prblmtc11 <prblmtc11@gmail.com>
2026-03-14 02:21:38 +0300

    changed main.cpp by connecting new module

--- yarmolinskaya.anna/S1/main.cpp
+++ yarmolinskaya.anna/S1/main.cpp
@@ -1,4 +1,12 @@
+#include "sequence.hpp"
+
 #include <iostream>
 
 int main()
-{}
+{
+  using namespace yarmolinskaya;
+  List< NamedSequence > data;
+  std::cout << "Reading sequence\n";
+  readSequences(data);
+  return 0;
+}
