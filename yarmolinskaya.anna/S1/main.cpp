9c69afee52aebb92e7717c2412220a451461dba9
prblmtc11 <prblmtc11@gmail.com>
2026-03-14 02:28:21 +0300

    implement processing

--- yarmolinskaya.anna/S1/main.cpp
+++ yarmolinskaya.anna/S1/main.cpp
@@ -1,16 +1,23 @@
 #include "sequence.hpp"
+
 #include <iostream>
 
 int main()
 {
   using namespace yarmolinskaya;
+
   List< NamedSequence > data;
+
   readSequences(data);
+
   if (data.empty())
   {
     std::cout << 0 << "\n";
     return 0;
   }
+
   printNames(data);
+  processSequences(data);
+
   return 0;
 }
