cf55a8066c5fbf089729c44343a1b4797e5ac963
prblmtc11 <prblmtc11@gmail.com>
2026-03-26 23:16:24 +0300

    added fix errors

--- yarmolinskaya.anna/S2/main.cpp
+++ yarmolinskaya.anna/S2/main.cpp
@@ -1,4 +1,48 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
-{}
+#include "expression.hpp"
+#include "common/list.hpp"
+
+int main(int argc, char* argv[])
+{
+  using namespace yarmolinskaya;
+
+  std::istream* input = &std::cin;
+  std::ifstream file;
+
+  if (argc > 1)
+  {
+    file.open(argv[1]);
+    input = &file;
+  }
+
+  List<long> results;
+  std::string line;
+
+  while (std::getline(*input, line))
+  {
+    if (line.empty())
+    {
+      continue;
+    }
+
+    results.push_front(evaluateExpression(line));
+  }
+
+  for (auto it = results.begin(); it != results.end(); ++it)
+  {
+    std::cout << *it;
+    auto next = it;
+    ++next;
+    if (next != results.end())
+    {
+      std::cout << " ";
+    }
+  }
+
+  std::cout << "\n";
+
+  return 0;
+}
